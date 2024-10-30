9ccce175bb4587fab3c7e04f9c89ead0b6fbbc63
Dennis <d-l-b@gmx.de>
2024-10-30 15:47:40 +0100

    solved dark mode challenge

--- js-dom-and-events_dark-mode/js/index.js
+++ js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+//Implements Dark Mode
+const buttonDarkMode = document.querySelector('[data-js="dark-mode-button"]');
+buttonDarkMode.addEventListener("click", () => {
+  bodyElement.classList.add("body-dark");
+});
+
+//Implements Light Mode
+const buttonLightMode = document.querySelector('[data-js="light-mode-button"]');
+buttonLightMode.addEventListener("click", () => {
+  bodyElement.classList.remove("body-dark");
+});
+
+//Implements Color Theme Toggle
+const buttonToggle = document.querySelector('[data-js="toggle-button"]');
+buttonToggle.addEventListener("click", () => {
+  bodyElement.classList.toggle("body-dark");
+});
