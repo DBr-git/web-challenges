ac21e7d2690a08093b1c146bc139305427921865
Dennis <d-l-b@gmx.de>
2024-10-30 16:08:07 +0100

    fixed formating

--- js-dom-and-events_dark-mode/js/index.js
+++ js-dom-and-events_dark-mode/js/index.js
@@ -1,21 +1,21 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const buttonDarkMode = document.querySelector('[data-js="dark-mode-button"]');
+const buttonLightMode = document.querySelector('[data-js="light-mode-button"]');
+const buttonToggle = document.querySelector('[data-js="toggle-button"]');
 
 //Implements Dark Mode
-const buttonDarkMode = document.querySelector('[data-js="dark-mode-button"]');
 buttonDarkMode.addEventListener("click", () => {
   bodyElement.classList.add("body-dark");
 });
 
 //Implements Light Mode
-const buttonLightMode = document.querySelector('[data-js="light-mode-button"]');
 buttonLightMode.addEventListener("click", () => {
   bodyElement.classList.remove("body-dark");
 });
 
 //Implements Color Theme Toggle
-const buttonToggle = document.querySelector('[data-js="toggle-button"]');
 buttonToggle.addEventListener("click", () => {
   bodyElement.classList.toggle("body-dark");
 });
