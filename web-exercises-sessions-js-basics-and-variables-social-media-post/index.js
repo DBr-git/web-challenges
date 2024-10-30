61e88a640bd73f75b6e4780fcac2943879142a63
Dennis <d-l-b@gmx.de>
2024-10-30 10:42:49 +0100

    solved social media post challenge

--- web-exercises-sessions-js-basics-and-variables-social-media-post/index.js
+++ web-exercises-sessions-js-basics-and-variables-social-media-post/index.js
@@ -11,6 +11,12 @@ console.clear();
 
 // --v-- write your code here --v--
 
+const title = "This is a title";
+const text = "This is a text";
+let likes = 20;
+const user = "UserX";
+const isReported = false;
+
 // --^-- write your code here --^--
 
 /*
@@ -18,5 +24,13 @@ console.clear();
 */
 
 // --v-- write your code here --v--
+
+console.log(title);
+console.log(text);
+console.log(likes);
+console.log(user);
+console.log(isReported);
+likes++;
+console.log(likes);
 
 // --^-- write your code here --^--
