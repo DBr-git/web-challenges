ec303209924a2657bba388b38e3764c8276ce5ed
Dennis <d-l-b@gmx.de>
2024-11-01 12:38:42 +0100

    solved typeof challenge

---
+++ js-conditions-and-booleans_log-type/js/index.js
@@ -0,0 +1,29 @@
+const data = 1;
+
+switch (typeof data) {
+  case "undefined":
+    console.log("undefined!");
+    break;
+  case "number":
+    console.log("number!");
+    break;
+  case "string":
+    console.log("string!");
+    break;
+  case "bigint":
+    console.log("bigInt!");
+    break;
+  case "function":
+    console.log("function!");
+    break;
+  case "symbol":
+    console.log("symbol!");
+    break;
+  case "object":
+    if (data === null) {
+      console.log("null!");
+    } else if (Array.isArray(data)) {
+      console.log("array!");
+    } else console.log("object!");
+    break;
+}
