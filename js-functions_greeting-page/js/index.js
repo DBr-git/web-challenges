7b2ba9ea3a6d841cc4202b1df46f031dd952e771
Dennis <d-l-b@gmx.de>
2024-11-04 16:07:42 +0100

    solved functions greeting challenge

--- js-functions_greeting-page/js/index.js
+++ js-functions_greeting-page/js/index.js
@@ -24,10 +24,30 @@ const display = document.querySelector('[data-js="display"]');
 
 function getGreeting() {
   // Code here
+  const currentTime = new Date().getHours();
+
+  if (currentTime >= 6 && currentTime <= 12) {
+    return "Good Morning";
+  } else if (currentTime >= 13 && currentTime <= 18) {
+    return "Good Afternoon";
+  } else if (currentTime >= 19 && currentTime <= 22) {
+    return "Good Evening";
+  } else if ((currentTime = 23 || currentTime <= 5)) {
+    return "Good Night";
+  }
 }
 
 function getDayColor() {
   // Code here
+  const weekday = new Date().getDay();
+
+  if (weekday == 1) {
+    return "darkgray";
+  } else if (weekday === 6 || weekday === 0) {
+    return "hotpink";
+  } else {
+    return "lightblue";
+  }
 }
 
 display.textContent = getGreeting();
