bad8d8679dc0cafcefbc765e452202af39e26048
Dennis <d-l-b@gmx.de>
2024-11-04 15:11:12 +0100

    solved functions with parameters challenge

--- js-functions_functions-with-parameters/js/index.js
+++ js-functions_functions-with-parameters/js/index.js
@@ -1,21 +1,18 @@
 console.clear();
 
-console.log("-------------");
-console.log("Exercise 1:");
+seperator("1");
 /*
 1: The function below is not executed. Can you figure out why? Find the bug and fix it.
 */
 
 // Uncomment the next line of code and figure out why it does not work:
-// logText;
+logText(); //Missing '()'
 
 function logText() {
   console.log("This text does not appear. Why?");
 }
 
-console.log("-------------");
-console.log("Exercise 2:");
-/*
+seperator("2"); /*
 2: Look at the functions below. They are almost identical. Can you find a way to generalize them into a new function with an input parameter? Replace the function calls below with your new function.
 */
 
@@ -31,18 +28,19 @@ function greetMary() {
   console.log("Welcome Mary, good to see you again!");
 }
 
-greetAlice();
-greetBob();
-greetMary();
+greet("Alice");
+greet("Bob");
+greet("Mary");
 
 // --v-- write your code here --v--
 
-function greet() {}
+function greet(name) {
+  console.log("Welcome " + name + ", good to see you again!");
+}
 
 // --^-- write your code here --^--
 
-console.log("-------------");
-console.log("Exercise 3:");
+seperator("3");
 /*
 3: We log a little separation text for each subtask. This is repetitive code, so lets write a function 'logSeparator' which takes the number of the exercise as a parameter and logs the respective separator. Then use this function to replace the existing separator logs in this javascript file.
 
@@ -50,5 +48,8 @@ Note: Separation text means the text that is logged into the console before each
 */
 
 // --v-- write your code here --v--
-
+function seperator(excercise) {
+  console.log("-------------");
+  console.log("Excercise " + excercise + ":");
+}
 // --^-- write your code here --^--
