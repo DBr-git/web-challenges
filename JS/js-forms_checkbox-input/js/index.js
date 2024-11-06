40d0abb2a3c89796fc40315943e450f50df163fe
Dennis <d-l-b@gmx.de>
2024-11-06 11:09:47 +0100

    solved chekcbox input challenge

--- JS/js-forms_checkbox-input/js/index.js
+++ JS/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const success = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,11 +13,19 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+hideTosError();
+success.hidden = true;
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
-
+  if (tosCheckbox.checked === false) {
+    showTosError();
+    return;
+  }
+  hideTosError();
+  success.hidden = false;
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
