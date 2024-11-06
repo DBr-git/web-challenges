fb5fc403b814651ecc84181e9fb357aaaf08b068
Dennis <d-l-b@gmx.de>
2024-11-06 10:51:31 +0100

    solved handle form submit challenge

--- JS/js-forms_handle-form-submit/js/index.js
+++ JS/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,22 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //Log Data
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  //Log Age-Badness-Sum
+  const ageBadnessSum =
+    Number(event.target.elements.age.value) +
+    Number(event.target.elements.badness.value);
+  console.log(ageBadnessSum);
+
+  //Reset Form
+  event.target.reset();
+  event.target.elements.firstName.focus();
+});
