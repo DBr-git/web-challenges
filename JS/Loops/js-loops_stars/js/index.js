0777d17220e67d909d7dabe0486aad4bde16cd6e
Dennis <d-l-b@gmx.de>
2024-11-07 12:14:08 +0100

    solved stars challenge

--- JS/Loops/js-loops_stars/js/index.js
+++ JS/Loops/js-loops_stars/js/index.js
@@ -2,12 +2,21 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
-
+  for (let i = 1; i <= 5; i++) {
+    const star = document.createElement("img");
+    filledStars < i
+      ? (star.src = "./assets/star-empty.svg")
+      : (star.src = "./assets/star-filled.svg");
+    star.addEventListener("click", () => {
+      renderStars(i);
+    });
+    starContainer.append(star);
+  }
   // --^-- write or modify code above this line --^--
 }
 
