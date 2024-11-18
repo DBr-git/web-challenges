8411e1391a26221547665cde584057bee1dd0c0e
Dennis <d-l-b@gmx.de>
2024-11-18 12:35:16 +0100

    fixed challenge mistake

--- react/react-basics/react-basics_hello-world-article/src/App.jsx
+++ react/react-basics/react-basics_hello-world-article/src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
   return (
     <article>
       <h1>Hello World</h1>
