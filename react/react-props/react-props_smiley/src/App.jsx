7d4cd0bc77b0f5d4d977dc07dd7b1794e8369443
Dennis <d-l-b@gmx.de>
2024-11-18 15:37:52 +0100

    solved smiley challenge

--- react/react-props/react-props_smiley/src/App.jsx
+++ react/react-props/react-props_smiley/src/App.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley isHappy />;
+}
+
+function Smiley({ isHappy }) {
+  return isHappy ? <h1>😀</h1> : <h1>☹️</h1>;
 }
