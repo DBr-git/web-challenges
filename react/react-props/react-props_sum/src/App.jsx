dccb685a04a64149c1754200c2c5bd1fb4fa4acf
Dennis <d-l-b@gmx.de>
2024-11-18 15:31:57 +0100

    solved sum challenge

--- react/react-props/react-props_sum/src/App.jsx
+++ react/react-props/react-props_sum/src/App.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA={20} valueB={10} />;
+}
+
+function Sum({ valueA, valueB }) {
+  return valueA + valueB;
 }
