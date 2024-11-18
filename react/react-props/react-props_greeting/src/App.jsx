3c71f81c3428f146fe19d153309608b465a473fc
Dennis <d-l-b@gmx.de>
2024-11-18 15:27:54 +0100

    solved greeting challenge

--- react/react-props/react-props_greeting/src/App.jsx
+++ react/react-props/react-props_greeting/src/App.jsx
@@ -1,5 +1,9 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Galadriel" />;
+}
+
+function Greeting({ name }) {
+  return name === "Niklas" ? <h1>Hello, Coach!</h1> : <h1>Hello, {name}!</h1>;
 }
