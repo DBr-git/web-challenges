abf89e9496c68cea55a647e0d0da555e6acf8e4c
Dennis <d-l-b@gmx.de>
2024-11-18 11:31:05 +0100

    solved attributes challenge

--- react/react-basics/react-basics_attributes/src/App.jsx
+++ react/react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,18 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return Article();
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Article Title</h2>
+      <lable htmlFor="article__input">Article Input</lable>
+      <input id="article__input"></input>
+      <a className="article__link" href="https://github.com/DBr-git">
+        Text content
+      </a>
+    </article>
+  );
 }
