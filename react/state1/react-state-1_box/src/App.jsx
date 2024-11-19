5883f682e402c22c401389176670360785a079a0
Dennis <d-l-b@gmx.de>
2024-11-19 15:38:52 +0100

    solved box challenge

--- react/state1/react-state-1_box/src/App.jsx
+++ react/state1/react-state-1_box/src/App.jsx
@@ -1,18 +1,22 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let isActive = false;
+  const [isActive, setIsActive] = useState(false);
+  console.log(isActive);
 
   function handleClick() {
-    isActive = !isActive;
+    setIsActive(!isActive);
     // Check that the value changes correctly.
-    console.log(isActive);
+    // console.log(active);
   }
 
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <button onClick={handleClick}>
+        {isActive ? "Deactivate" : "Activate"}
+      </button>
     </main>
   );
 }
