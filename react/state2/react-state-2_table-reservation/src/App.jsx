5d97fa74e540363a99be01c66e9837da6984a72d
Dennis <d-l-b@gmx.de>
2024-11-20 16:19:15 +0100

    solved reservation challenge

--- react/state2/react-state-2_table-reservation/src/App.jsx
+++ react/state2/react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,27 @@
 import Counter from "./components/Counter";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [people, setPeople] = useState(1);
+
+  function handleAddPeople() {
+    setPeople(people + 1);
+  }
+
+  function handleDeletePeople() {
+    setPeople(people - 1);
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter
+        onAddPeople={handleAddPeople}
+        onDeletePeople={handleDeletePeople}
+        people={people}
+      />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
