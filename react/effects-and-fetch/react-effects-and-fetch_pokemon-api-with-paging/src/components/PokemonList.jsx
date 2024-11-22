532c09b3d69e816b084d69959fbba62278841fee
Dennis <d-l-b@gmx.de>
2024-11-22 11:55:17 +0100

    solved pokemon paging challenge

--- react/effects-and-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
+++ react/effects-and-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
@@ -1,30 +1,55 @@
 import { useEffect, useState } from "react";
 
 export default function PokemonList() {
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemon, setPokemon] = useState(null);
+  const [page, setPage] = useState(
+    "https://pokeapi.co/api/v2/pokemon?offset=0"
+  );
+
+  function handleNextPage() {
+    if (pokemon?.next) {
+      setPage(pokemon.next);
+    }
+  }
+
+  function handlePreviousPage() {
+    if (pokemon?.previous) {
+      setPage(pokemon.previous);
+    }
+  }
 
   useEffect(() => {
     async function loadPokemon() {
       try {
-        const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
-        );
+        const response = await fetch(page);
         const data = await response.json();
-        setPokemon(data.results);
+        setPokemon(data);
       } catch (error) {
         console.log(error);
       }
     }
 
     loadPokemon();
-  }, []);
+  }, [page]);
+
+  if (!pokemon) {
+    return null;
+  }
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button
+        type="button"
+        onClick={handlePreviousPage}
+        disabled={!pokemon.previous}
+      >
+        Previous Page
+      </button>
+      <button type="button" onClick={handleNextPage} disabled={!pokemon.next}>
+        Next Page
+      </button>
       <ul>
-        {pokemon.map(({ name }) => (
+        {pokemon.results.map(({ name }) => (
           <li key={name}>{name}</li>
         ))}
       </ul>
