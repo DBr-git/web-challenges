de99180898a720f80cfa47aa404e1f3b03ef0ca6
Dennis <d-l-b@gmx.de>
2024-12-02 11:43:18 +0100

    solved animal challenge

--- unit-testing/js-unit-testing_get-animal/index.js
+++ unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,10 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (!animal) {
+    return "I do not like animals at all!";
+  }
+  if (animal === "dogs") {
+    return "I like dogs!";
+  } else {
+    return "I totally love cats!";
+  }
+}
