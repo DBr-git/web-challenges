9dd96aff71366d81f42ffd9ea2e1d24cde760268
Dennis <d-l-b@gmx.de>
2024-12-02 17:11:15 +0100

    worked on scorekeeper challenge

--- react/component-testing/react-component-testing_scorekeeper/components/GameForm/GameForm.test.js
+++ react/component-testing/react-component-testing_scorekeeper/components/GameForm/GameForm.test.js
@@ -8,10 +8,52 @@ jest.mock("next/router", () => ({
   },
 }));
 
-test("renders two input fields and a button", () => {});
+test("renders two input fields and a button", () => {
+  render(<GameForm />);
 
-test("renders a form with the accessible name 'Create a new game'", () => {});
+  const input = screen.getAllByRole("textbox");
+  expect(input).toHaveLength(2);
 
-test("submits the correct form data when every field is filled out", async () => {});
+  const button = screen.getByRole("button", { name: /create game/i });
+  expect(button).toBeInTheDocument();
+});
 
-test("does not submit form if one input field is left empty", async () => {});
+test("renders a form with the accessible name 'Create a new game'", () => {
+  render(<GameForm />);
+
+  const form = screen.getByRole("form", { name: /Create a new game/i });
+  expect(form).toBeInTheDocument();
+});
+
+test("submits the correct form data when every field is filled out", async () => {
+  const user = userEvent.setup();
+  const onCreateGame = jest.fn();
+
+  render(<GameForm onCreateGame={onCreateGame} />);
+
+  const input1 = screen.getByLabelText(/Name of Game/i);
+  const input2 = screen.getByLabelText(/Player names, separated by comma/i);
+  const button = screen.getByRole("button", { name: /create game/i });
+
+  await user.type(input1, "TestGame");
+  await user.type(input2, "Player1, Player2");
+  await user.click(button);
+
+  expect(onCreateGame).toHaveBeenCalledWith({
+    nameOfGame: "TestGame",
+    playerNames: ["Player1", "Player2"],
+  });
+});
+
+test("does not submit form if one input field is left empty", async () => {
+  const user = userEvent.setup();
+  const onCreateGame = jest.fn();
+
+  render(<GameForm onCreateGame={onCreateGame} />);
+
+  const button = screen.getByRole("button", { name: /create game/i });
+
+  await user.click(button);
+
+  expect(onCreateGame).not.toHaveBeenCalled();
+});
--- react/component-testing/react-component-testing_scorekeeper/components/Input/Input.test.js
+++ react/component-testing/react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -2,6 +2,24 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  const onChange = jest.fn();
+
+  render(
+    <Input
+      labelText="Test"
+      placeholder="Placeholder"
+      name="nameOfGame"
+      value="testValue"
+      onChange={onChange}
+    />
+  );
+
+  const input = screen.getByLabelText("Test");
+  expect(input).toBeInTheDocument();
+  expect(input).toHaveAttribute("placeholder", "Placeholder");
+  expect(input).toHaveAttribute("name", "nameOfGame");
+  expect(input).toHaveAttribute("value", "testValue");
+});
 
 test("calls callback on every user input", async () => {});
